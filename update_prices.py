432fa6364aee6054c16b5e619485a430d944bab3
Jorge Aranda <jarandag@gmail.com>
2017-03-18 16:34:12 -0700

    Modify update_prices script

--- update_prices.py
+++ update_prices.py
@@ -1,29 +1,55 @@
 from docopt import docopt
+import pandas as pd
+import psycopg2
+from psycopg2.extras import NamedTupleCursor
 
+import config
 
 usage = '''
 Load missing asset prices from Yahoo Finance into the database.
 
 Usage:
-    update_prices.py [-h] [-v] [--env <env>]
+    update_prices.py [-h] [--env <env>]
 
 Options:
     -h --help               Show this
     -e <env> --env <env>    Environment to load prices into [default: dev]
-    -v --verbose            Verbose output
 '''
+
+conn = None
+
+
+def connect(env):
+    global conn
+    conn = psycopg2.connect(database=config.database[env], cursor_factory=NamedTupleCursor)
+
+
+def get_tickers():
+    with conn.cursor() as cur:
+        cur.execute('''SELECT ticker FROM assets WHERE class != 'Domestic Cash';''')
+        return cur.fetchall()
 
 
 def main(args):
-    # Connect to database
-    # Load assets data
-    # For each asset,
-    #   request prices from API
-    #   load days with asset price currently missing
-    #   for each market day with missing price,
-    #       find the price from the API
-    #       update the database
-    pass
+    env = args['--env']
+    connect(env)
+    prices = {x.ticker: None for x in get_tickers()}
+    cur = conn.cursor()
+    for ticker in prices.keys():
+        table = pd.read_csv('http://ichart.finance.yahoo.com/table.csv?s=' + ticker + '&g=d')
+        for row in table.iterrows():
+            if row[1]['Date'] >= '2016-09-08':  # TODO: so hacky; fix
+                cur.execute('''
+                    INSERT INTO assetprices (ticker, day, ask, bid)
+                    VALUES (%(ticker)s, %(day)s, %(close)s, %(close)s) ON CONFLICT DO NOTHING;''',
+                    {
+                        'ticker': ticker,
+                        'day': row[1]['Date'],
+                        'close': row[1]['Close']
+                    })
+
+    cur.close()
+    conn.commit()
 
 
 if __name__ == '__main__':
