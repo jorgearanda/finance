0ff30e5d590ec83d5932fb0d9eebc1067b868585
Jorge Aranda <jarandag@gmail.com>
2017-04-05 22:08:00 -0700

    Add Tickers class

---
+++ components/tickers.py
@@ -0,0 +1,89 @@
+from datetime import date
+import pandas as pd
+
+from components.ticker import Ticker
+from db import db
+
+
+class Tickers():
+    """DataFrame-based structure that wraps several Ticker objects
+
+    Instance variables:
+    ticker_names -- List with the ticker names contained in the object
+    tickers -- Dict with ticker names as keys and Ticker objects as values
+    prices -- DataFrame indexed by day with one column per ticker. Values represent daily ticker prices
+    changes -- DataFrame indexed by day with one column per ticker. Values represent daily price changes
+    changes_from_start -- DataFrame indexed by day with one column per ticker. Values represent price changes since
+        the first day
+    volatilities -- Dict of ticker volatilities (standard deviation of price changes)
+    correlations -- DataFrame indexed by ticker name with one column per ticker. Values represent the correlation
+        between both tickers' prices
+    """
+
+    def __init__(self, from_day=None):
+        """Instantiate a Tickers object.
+
+        The Tickers object wraps individual Ticker objects, and summarizes data for the collection of tickers.
+        As is convention elsewhere in the code, a `from_day` value of None
+        will extract all available dates and prices for the set of tickers.
+
+        Keyword arguments:
+        from_day -- the first day in the sequence, in datetime.date format (default None)
+        """
+        self.ticker_names = self._get_ticker_names(from_day)
+        self.tickers = self._get_tickers(from_day)
+        if len(self.ticker_names) > 0:
+            self.prices = self._collect_prices()
+            self.changes = self._collect_changes()
+            self.changes_from_start = self._collect_changes_from_start()
+            self.volatilities = self._collect_volatilities()
+            self.correlations = self._calc_correlations()
+
+    def __repr__(self):
+        return str(self.tickers)
+
+    def __str__(self):
+        return str(self.tickers)
+
+    def _get_ticker_names(self, from_day):
+        """Get all ticker names for which there are prices available since from_day."""
+        db.ensure_connected()
+        with db.conn.cursor() as cur:
+            cur.execute('''
+                SELECT DISTINCT(ticker) AS name
+                FROM assetprices
+                WHERE (%(from_day)s IS NULL OR day >= %(from_day)s) AND day < %(today)s
+                ORDER BY name ASC;''',
+                {'from_day': from_day, 'today': date.today()})
+
+            return [x.name for x in cur.fetchall()]
+
+    def _get_tickers(self, from_day):
+        """Get all ticker objects."""
+        return {name: Ticker(name, from_day) for name in self.ticker_names}
+
+    def _collect_prices(self):
+        """Extract the prices from the ticker objects and collect them in a single DataFrame."""
+        _prices = pd.concat([self.tickers[name].prices['price'] for name in self.ticker_names], axis=1)
+        _prices.columns = self.ticker_names
+        return _prices
+
+    def _collect_changes(self):
+        """Extract the price changes from the ticker objects and collect them in a single DataFrame."""
+        _changes = pd.concat([self.tickers[name].prices['change'] for name in self.ticker_names], axis=1)
+        _changes.columns = self.ticker_names
+        return _changes
+
+    def _collect_changes_from_start(self):
+        """Extract the price changes from the start from the ticker objects and collect them in a single DataFrame."""
+        _changes = pd.concat([self.tickers[name].prices['change_from_start'] for name in self.ticker_names], axis=1)
+        _changes.columns = self.ticker_names
+        return _changes
+
+    def _collect_volatilities(self):
+        """Extract the volatilities from all tickers and collect them in a dictionary."""
+        return {name: self.tickers[name].volatility for name in self.ticker_names}
+
+    def _calc_correlations(self):
+        """Calculate the correlations between ticker prices."""
+        return self.prices.corr()
---
+++ test/test_components/test_tickers.py
@@ -0,0 +1,40 @@
+from datetime import datetime as dt, date
+from freezegun import freeze_time
+import pandas as pd
+from pytest import approx
+
+from components.tickers import Tickers
+from db import db
+from test.fixtures import simple
+
+
+def setup_function():
+    db._env = 'test'
+    db.ensure_connected()
+
+
+def test_tickers_class_instantiates():
+    t = Tickers()
+    assert t is not None
+    assert t.__repr__() == '{}'
+    assert str(t) == '{}'
+
+
+def test_loads_with_data(simple):
+    with freeze_time(dt(2017, 3, 7)):
+        t = Tickers()
+
+    assert len(t.ticker_names) == 2
+    assert t.ticker_names[0] == 'VCN.TO'
+    assert t.ticker_names[1] == 'VEE.TO'
+
+    assert len(t.tickers) == 2
+
+    assert t.prices.columns[0] == 'VCN.TO'
+    assert t.prices.columns[1] == 'VEE.TO'
+    assert t.prices.loc[date(2017, 3, 2)]['VCN.TO'] == approx(30.00)
+    assert t.changes.loc[date(2017, 3, 3)]['VCN.TO'] == approx(30.10 / 30.00 - 1)
+    assert t.changes_from_start.loc[date(2017, 3, 6)]['VCN.TO'] == approx(29.85 / 30.00 - 1)
+    assert t.volatilities['VEE.TO'] == t.tickers['VEE.TO'].volatility
+    assert t.correlations['VCN.TO']['VCN.TO'] == approx(1.00)
+    assert t.correlations['VCN.TO']['VEE.TO'] == approx(-0.988212)
