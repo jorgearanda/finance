d8cb22498b1f343b32cb750258866d55b3cb75b5
Jorge Aranda <jarandag@gmail.com>
2017-07-29 22:22:33 -0700

    Clean up Position component
    Tests still missing.

--- components/position.py
+++ components/position.py
@@ -30,25 +30,17 @@ class Position():
         self.ticker_name = ticker_name
         self.account = account
         self._from_day = from_day
-        _ticker = Ticker(ticker_name, from_day)
-        _current_prices = _ticker.values['price']
-        _units_and_costs = self._get_units_and_costs(_ticker.values.index)
-        _costs_per_unit = self._calc_costs_per_unit(_units_and_costs)
-        _appreciations = self._calc_appreciations(_ticker, _units_and_costs)
-        _distributions = self._get_distributions(_units_and_costs)
+        self._ticker = Ticker(ticker_name, from_day)
+        self.values = pd.DataFrame(
+            index=self._ticker.values.index,
+            columns=['units', 'cost', 'cost_per_unit', 'current_price', 'market_value', 'open_profit', 'distributions',
+                'distribution_returns', 'appreciation_returns', 'total_returns'])
 
-        self.values = pd.DataFrame({
-            'current_price': _current_prices,
-            'units': _units_and_costs['units'],
-            'cost': _units_and_costs['cost'],
-            'cost_per_unit': _costs_per_unit['cost_per_unit'],
-            'market_value': _appreciations['market_value'],
-            'open_profit': _appreciations['open_profit'],
-            'distributions': _distributions['distributions'],
-            'distribution_returns': _distributions['distribution_returns'],
-            'appreciation_returns': _appreciations['appreciation_returns'],
-            'total_returns': _distributions.fillna(0)['distribution_returns'] + _appreciations['appreciation_returns']
-        })
+        self.values['current_price'] = self._ticker.values['price']
+        self._get_units_and_costs()
+        self._calc_appreciations()
+        self._get_distributions()
+        self._calc_total_returns()
 
     def __repr__(self):
         return str(self.values.head())
@@ -56,11 +48,7 @@ class Position():
     def __str__(self):
         return str(self.values.head())
 
-    def _say_hi(self):
-        print('hi')
-
-    def _get_units_and_costs(self, by_day):
-        frame = pd.DataFrame(index=by_day, columns=['units', 'cost'])
+    def _get_units_and_costs(self):
         db.ensure_connected()
         with db.conn.cursor() as buys:
             buys.execute('''
@@ -81,43 +69,29 @@ class Position():
             last_units = 0.0
             last_cost = 0.0
 
-            for day, row in frame.iterrows():
-                row['units'] = last_units
-                row['cost'] = last_cost
+            for day, row in self.values.iterrows():
+                self.values.loc[day, 'units'] = last_units
+                self.values.loc[day, 'cost'] = last_cost
                 while next_buy is not None and day == next_buy.day:
-                    row['units'] += float(next_buy.units)
-                    row['cost'] += float(next_buy.total)
+                    self.values.loc[day, 'units'] += float(next_buy.units)
+                    self.values.loc[day, 'cost'] += float(next_buy.total)
                     next_buy = buys.fetchone()
 
-                last_units = row['units']
-                last_cost = row['cost']
-                row['cost_per_unit'] = float('nan') if last_units == 0 else last_cost / last_units
+                last_units = self.values.loc[day, 'units']
+                last_cost = self.values.loc[day, 'cost']
+                self.values.loc[day, 'cost_per_unit'] = float('nan') if last_units == 0 else last_cost / last_units
 
-        return frame
+    def _calc_appreciations(self):
+        for day, row in self.values.iterrows():
+            self.values.loc[day, 'market_value'] = self.values.loc[day, 'units'] * self._ticker.price(day)
+            self.values.loc[day, 'open_profit'] = self.values.loc[day, 'market_value'] - self.values.loc[day, 'cost']
+            if self.values.loc[day, 'cost'] == 0:
+                self.values.loc[day, 'appreciation_returns'] = float('nan')
+            else:
+                self.values.loc[day, 'appreciation_returns'] = \
+                    self.values.loc[day, 'open_profit'] / self.values.loc[day, 'cost']
 
-    def _calc_costs_per_unit(self, _units_and_costs):
-        _costs_per_unit = pd.DataFrame(_units_and_costs, columns=['cost_per_unit'])
-        for day, row in _units_and_costs.iterrows():
-            _costs_per_unit.loc[day]['cost_per_unit'] = \
-                float('nan') if row['units'] == 0 else row['cost'] / row['units']
-
-        return _costs_per_unit
-
-    def _calc_appreciations(self, _ticker, _units_and_costs):
-        _appreciations = pd.DataFrame(
-            index=_units_and_costs.index,
-            columns=['market_value', 'open_profit', 'appreciation_returns'])
-        for day, row in _units_and_costs.iterrows():
-            _appreciations.loc[day]['market_value'] = row['units'] * _ticker.price(day)
-            _appreciations.loc[day]['open_profit'] = _appreciations.loc[day]['market_value'] - row['cost']
-            _appreciations.loc[day]['appreciation_returns'] = \
-                float('nan') if row['cost'] == 0 else _appreciations.loc[day]['open_profit'] / row['cost']
-
-        return _appreciations
-
-    def _get_distributions(self, _units_and_costs):
-        _distributions = pd.DataFrame(index=_units_and_costs.index, columns=['distributions', 'distribution_returns'])
-
+    def _get_distributions(self):
         db.ensure_connected()
         with db.conn.cursor() as dividends:
             dividends.execute('''
@@ -137,14 +111,18 @@ class Position():
             next_dividend = dividends.fetchone()
             last_distribution = 0.0
 
-            for day, row in _distributions.iterrows():
-                row['distributions'] = last_distribution
+            for day, row in self.values.iterrows():
+                self.values.loc[day, 'distributions'] = last_distribution
                 while next_dividend is not None and day == next_dividend.day:
-                    row['distributions'] += float(next_dividend.total)
+                    self.values.loc[day, 'distributions'] += float(next_dividend.total)
                     next_dividend = dividends.fetchone()
 
-                last_distribution = row['distributions']
-                row['distribution_returns'] = \
-                    float('nan') if last_distribution == 0 else last_distribution / _units_and_costs.loc[day]['cost']
+                last_distribution = self.values.loc[day, 'distributions']
+                if self.values.loc[day, 'cost'] == 0:
+                    self.values.loc[day, 'distribution_returns'] = float('nan')
+                else:
+                    self.values.loc[day, 'distribution_returns'] = last_distribution / self.values.loc[day, 'cost']
 
-        return _distributions
+    def _calc_total_returns(self):
+        self.values['total_returns'] = \
+            self.values.fillna(0)['distribution_returns'] + self.values.fillna(0)['appreciation_returns']
