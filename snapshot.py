a837e6c6679a574db117922203f58b7e070ce647
Jorge Aranda <jorge.aranda@cuevano.ca>
2018-04-24 12:09:06 -0700

    Add current month stats to snapshot

--- snapshot.py
+++ snapshot.py
@@ -4,6 +4,8 @@ from portfolio import Portfolio
 p = Portfolio(verbose=False)
 latest = p.latest()
 last_month = p.latest_month()
+month_profit = latest['profit'] - last_month['profit']
+month_returns = month_profit / last_month['total_value']
 
 print('Portfolio Snapshot')
 print('===========================================================')
@@ -18,9 +20,12 @@ print(
     f'MWRR Annual:  {latest["mwrr_annualized"] * 100:12,.2f}%')
 print(f'Total Returns:{latest["returns"] * 100:12,.2f}%')
 print()
+print(f'Day Profit:   {latest["day_profit"]:12,.2f}')
+print(f'Day Returns:  {latest["day_returns"] * 100:12,.2f}%')
+print()
 print(
-    f'Day Profit:   {latest["day_profit"]:12,.2f}      ' +
-    f'Month Profit: {last_month["month_profit"]:12,.2f}')
+    f'Month Profit: {month_profit:12,.2f}      ' +
+    f'Last Month:   {last_month["month_profit"]:12,.2f}')
 print(
-    f'Day Returns:  {latest["day_returns"] * 100:12,.2f}%     ' +
-    f'Month Returns:{last_month["month_returns"] * 100:12,.2f}%')
+    f'Month Returns:{month_returns * 100:12,.2f}%     ' +
+    f'Last Month:   {last_month["month_returns"] * 100:12,.2f}%')
