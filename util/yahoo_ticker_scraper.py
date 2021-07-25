a1f2d7c5ec304d8992b307dfd335a794b712038e
Jorge Aranda <jorge.aranda@cuevano.ca>
2021-07-24 20:36:14 -0700

    Split Yahoo ticker scraper into its own file

---
+++ util/yahoo_ticker_scraper.py
@@ -0,0 +1,91 @@
+import calendar
+import grequests
+import re
+import requests
+
+from datetime import date, datetime as dt, timedelta
+
+
+class YahooTickerScraper:
+    auth_url = "https://finance.yahoo.com/quote/VAB.TO/history?p=VAB.TO"
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    timeout = 5  # seconds
+
+    def __init__(self, verbose=False, look_back_days=30):
+        self.verbose = verbose
+        self.look_back_days = look_back_days
+        self.cookie, self.crumb = self._get_cookie_and_crumb()
+
+    def get_ticker_prices(self, symbols):
+        """
+        Fetch price data from Yahoo for the symbols requested.
+
+        Returns a dictionary with symbol keys and price values
+        (in the form of nested day: price dictionaries).
+        """
+        responses = self._get_ticker_requests(symbols)
+        tickers = {}
+        for res in responses:
+            symbol, prices = self._extract_prices(res)
+            tickers[symbol] = prices
+        return tickers
+
+    def _get_cookie_and_crumb(self):
+        if self.verbose:
+            print("* Getting cookie and crumb")
+        req = requests.get(
+            self.auth_url,
+            headers={"User-Agent": self.user_agent},
+            timeout=self.timeout,
+        )
+        cookie = {"B": req.cookies["B"]}
+        content = req.content.decode("unicode-escape")
+        match = re.search(r'CrumbStore":{"crumb":"(.*?)"}', content)
+        crumb = match.group(1)
+
+        return cookie, crumb
+
+    def _get_ticker_requests(self, symbols):
+        if self.verbose:
+            print("* Getting tickers")
+        return grequests.map(
+            grequests.get(
+                f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}"
+                f"?period1={self._ts_from()}&period2={self._ts_to()}&"
+                f"interval=1d&events=historical&crumb={self.crumb}",
+                headers={"User-Agent": self.user_agent},
+                cookies=self.cookie,
+                timeout=self.timeout,
+            )
+            for symbol in symbols
+        )
+
+    def _ts_from(self):
+        return calendar.timegm(
+            (date.today() - timedelta(days=self.look_back_days)).timetuple()
+        )
+
+    def _ts_to(self):
+        return calendar.timegm((date.today() + timedelta(days=1)).timetuple())
+
+    def _extract_prices(self, res):
+        symbol, price_lines = self._extract_price_lines(res)
+        prices = {
+            self._day_from_price_line(line): self._closing_price_from_price_line(line)
+            for line in price_lines
+            if len(line) > 0
+        }
+        return symbol, prices
+
+    def _extract_price_lines(self, res):
+        symbol = re.search(r"download/(.*)\?", res.request.url).group(1)
+        price_lines = res.text.split("\n")[1:]
+        return symbol, price_lines
+
+    def _day_from_price_line(self, line):
+        return dt.strptime(line.split(",")[0], "%Y-%m-%d").date()
+
+    def _closing_price_from_price_line(self, line):
+        closing_price = line.split(",")[4]
+        return float(closing_price) if closing_price != "null" else None
