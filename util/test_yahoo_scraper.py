299518f94690cbcb8a108b27764297f400feb8a7
Jorge Aranda <jorge.aranda@cuevano.ca>
2021-07-29 18:27:55 -0700

    Test Yahoo quote parsing

--- util/test_yahoo_scraper.py
+++ util/test_yahoo_scraper.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from util.yahoo_scraper import Quote
 from util.yahoo_scraper import YahooScraper
 
 
@@ -5,3 +8,10 @@ def test_yahoo_scraper_initializes():
     y = YahooScraper()
     assert y.look_back_days == 30
     assert y.cookie is None
+
+
+def test_quote_class():
+    q = Quote("SYM", "2021-07-29,77.22,77.42,77.15,77.15,77.14,56026")
+    assert q.symbol == "SYM"
+    assert q.day == date(2021, 7, 29)
+    assert q.price == 77.15
