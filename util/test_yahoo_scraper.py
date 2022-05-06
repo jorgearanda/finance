198fd6ac9e43098db306be3955fdf636b4c12182
Jorge Aranda <jorge.aranda@cuevano.ca>
2022-05-06 15:22:04 -0700

    Fix a test

--- util/test_yahoo_scraper.py
+++ util/test_yahoo_scraper.py
@@ -7,7 +7,7 @@ from util.yahoo_scraper import YahooScraper
 def test_yahoo_scraper_initializes():
     y = YahooScraper()
     assert y.look_back_days == 30
-    assert y.cookie is None
+    assert y.crumb is None
 
 
 def test_quote_class():
