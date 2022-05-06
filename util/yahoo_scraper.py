e9da0e58002cfaec0fa264383daff5ecee6f0b18
Jorge Aranda <jorge.aranda@cuevano.ca>
2022-05-06 10:05:50 -0700

    Revert "Remove cookie and crumb code from Yahoo scraper"
    This reverts commit fca9ad53b4a2514c825abb2d99fd7aafbf9221c1.

--- util/yahoo_scraper.py
+++ util/yahoo_scraper.py
@@ -7,13 +7,7 @@ from datetime import date, datetime as dt, timedelta
 
 
 class YahooScraper:
-    """
-    The current version of the Yahoo Finance site does not require cookies or
-    crumbs.
-
-    Earlier versions of this scraper had code to extract those values so I
-    could query the site for CSV files.
-    """
+    auth_url = "https://finance.yahoo.com/quote/VAB.TO/history?p=VAB.TO"
     user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
     timeout = 5  # seconds
@@ -28,19 +22,35 @@ class YahooScraper:
 
         Returns a list of Quote objects with symbol, day, and price attributes.
         """
+        if self.cookie is None:
+            self.cookie, self.crumb = self._get_cookie_and_crumb()
         responses = self._fetch_quotes(symbols)
         quotes = []
         for res in responses:
             quotes.extend(self._parse_quotes(res))
         return quotes
 
+    def _get_cookie_and_crumb(self):
+        req = requests.get(
+            self.auth_url,
+            headers={"User-Agent": self.user_agent},
+            timeout=self.timeout,
+        )
+        cookie = {"B": req.cookies["B"]}
+        content = req.content.decode("unicode-escape")
+        match = re.search(r'CrumbStore":{"crumb":"(.*?)"}', content)
+        crumb = match.group(1)
+
+        return cookie, crumb
+
     def _fetch_quotes(self, symbols):
         return grequests.map(
             grequests.get(
                 f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}"
                 f"?period1={self._ts_from()}&period2={self._ts_to()}&"
-                f"interval=1d&events=historical",
+                f"interval=1d&events=historical&crumb={self.crumb}",
                 headers={"User-Agent": self.user_agent},
+                cookies=self.cookie,
                 timeout=self.timeout,
             )
             for symbol in symbols
